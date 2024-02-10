91c6c489c5b67ae5bcea25bc3e3beed4bab79e3b
Daniel Ribeiro <109773063+DevDanielJR@users.noreply.github.com>
2024-02-09 21:47:18 -0300

    Desafio01
    Desafio de agendamento de pagamento.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # desafios-de-codigo-da-dio
+
+Olá, sou um estudante de programação cujo está fazendo o bootcamp da DIO no momento, criei esse repositório para armazenar nele os desafios de código que irei fazer. 
+Estou aberto à Pull-Request caso você observou alguma melhoria que eu poderia ter feito. Mas lembre-se de ser respeitoso na hora de solicitar pull-request, tenha paciência comigo. Rs.
---
+++ agendamento-de-pagamento.js
@@ -0,0 +1,19 @@
+
+let nomeBeneficiario;
+let valorConta;
+let dataVencimento;
+
+// Neste bloco, temos as funções gets para ler os valores de entrada:
+nomeBeneficiario = "Marcelo";
+valorConta = 1000;
+dataVencimento = "31/12/2023";
+
+// TODO: Imprima a saída adequada utilizando o conceito de template strings:
+console.log(`Pagamento Agendado! Valor: R$ ${valorConta}, vencimento ${dataVencimento}.`);
+
+//Dado de entrada:
+//Marcelo
+//1000
+//31/12/2023
+//Saída esperada:
+//Pagamento Agendado! Valor: R$ 1000, vencimento 31/12/2023.
