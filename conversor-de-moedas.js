fa99a5e6cb5a771ccfaa9305cc885b2cbe4d34a0
Daniel Ribeiro <109773063+DevDanielJR@users.noreply.github.com>
2024-02-10 20:16:43 -0300

    Conversor de Moedas
    Exercício para realizar a conversão de moedas.

---
+++ conversor-de-moedas.js
@@ -0,0 +1,43 @@
+
+//Desafios JavaScript na DIO têm funções "gets" e "print" acessíveis globalmente:
+//- "gets" : lê UMA linha com dado(s) de entrada (inputs) do usuário;
+//- "print": imprime um texto de saída (output), pulando linha.
+
+//Saiba mais sobre isNaN(): https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/isNaN
+
+const valorDinheiro = parseFloat(gets(2000));
+const moedaOrigem = "BRL";
+const moedaDestino = "USD";
+
+// Aqui é verificado se valorDinheiro é um número.
+// !isNaN é uma função embutida em JavaScript que verifica se o valor passado como argumento não é um número.
+if (!isNaN(valorDinheiro)) {
+// Definição fixa das taxas de câmbio para as moedas:
+  const taxaCambioBRLtoUSD = 0.1875;
+  const taxaCambioBRLtoEUR = 0.1520;
+  const taxaCambioUSDtoBRL = 5.3333;
+  const taxaCambioEURtoBRL = 6.5789;
+
+  let valorConvertido;
+
+//TODO: Crie uma estrutura condicional que realize a conversão das moedas:
+  if (moedaOrigem === 'BRL' && moedaDestino === 'USD') {
+    valorConvertido = valorDinheiro * taxaCambioBRLtoUSD;
+  } else if (moedaOrigem === 'BRL' && moedaDestino === 'EUR') {
+    valorConvertido = valorDinheiro * taxaCambioBRLtoEUR;
+  } else if (moedaOrigem === 'USD' && moedaDestino === 'BRL') {
+    valorConvertido = valorDinheiro * taxaCambioUSDtoBRL;
+  } else if (moedaOrigem === 'EUR' && moedaDestino === 'BRL') {
+    valorConvertido = valorDinheiro * taxaCambioEURtoBRL;
+  } 
+
+//TODO: Imprima a conversão com duas casas decimais:
+  console.log(`${valorDinheiro} ${moedaOrigem} equivalem a ${valorConvertido.toFixed(2)} ${moedaDestino}.`);
+}
+
+//Dado de entrada:
+//2000
+//BRL
+//USD
+//Saída esperada:
+//2000 BRL equivalem a 375.00 USD.
