3f2181b5449d4a4f068003a8f1e33cd993c001f7
Daniel Ribeiro <109773063+DevJuvenilson@users.noreply.github.com>
2024-07-13 15:16:35 -0300

    finish whatever

--- sla.js
+++ sla.js
@@ -1,7 +1,17 @@
-
 //Desafios JavaScript na DIO têm funções "gets" e "print" acessíveis globalmente:
 //- "gets" : lê UMA linha com dado(s) de entrada (inputs) do usuário;
 //- "print": imprime um texto de saída (output), pulando linha.
+
+function gets(array, index) {
+    if (index < 0 || index >= array.length) {
+        return undefined; // ou qualquer lógica de erro
+    }
+    return array[index];
+}
+
+function print(texto) {
+    return console.log(texto)
+}
 
 // TODO: Crie as variáveis titulo, descricao e dataVencimento para guardar as informações das tarefas:
 const titulo = [
@@ -20,32 +30,22 @@ const dataDeVencimento = [
     "22/12",
 ]
 
-
-function print(texto) {
-    return console.log(texto);
-}
-
 // Obtém o titulo e a descricao a partir da entrada do console  
-function indexOfVar(nomeVariavel) {
-    for (let i = 0; i < nomeVariavel.length; i++) {
-        var valorDoIndex = nomeVariavel[i];
-    }
+const titulo1 = titulo[0];
+const descricao1 = descricao[0];
+const descricaoLenght = descricao1.length;
+const dataDeVencimento1 = gets(dataDeVencimento, 0);
 
-    return valorDoIndex
+// TODO: Crie uma estrutura condicional 'if/else' para verificar se a descrição da tarefa excede 50 caracteres.
+function verificarDescricaoLenght (descricaoLenght, descricao, dataDeVencimento) {
+  if (descricaoLenght > 50) {
+    return ('Descricao ultrapassa limite de caracteres.');
+  }
+
+  if (descricaoLenght <= 50) {
+    return (`${descricao} ate ${dataDeVencimento}`);
+  }
 }
 
-// // TODO: Crie uma estrutura condicional 'if/else' para verificar se a descrição da tarefa excede 50 caracteres.
-// function verificarDescricaoLenght(descricaoLenght, descricao, dataDeVencimento) {
-//     if (descricaoLenght > 50) {
-//         return ('Descricao ultrapassa limite de caracteres.');
-//     }
-
-//     if (descricaoLenght <= 50) {
-//         return (`${descricao} ate ${dataDeVencimento}`);
-//     }
-// }
-
-// // TODO: Imprima a descricao e a data de vencimento como nos exemplos da tabela:         
-// print(verificarDescricaoLenght(dataDeVencimento));
-
-print(indexOfVar(descricao))
+ // TODO: Imprima a descricao e a data de vencimento como nos exemplos da tabela:         
+print(verificarDescricaoLenght(descricaoLenght, descricao1, dataDeVencimento1));
--- teste.js
+++ teste.js
@@ -1,7 +1,46 @@
-const dataDeVencimento = [
-    "",
-    "18/12",
-    "22/12",
-]
+const atividades = [
+    {
+        id: 1,
+        titulo: "Backup Noturno",
+        descricao: "Executar backup automatico de servidores as 2h da madrugada",
+        dataDeVencimento: "",
+    },
+    {
+        id: 2,
+        titulo: "Monitoramento de Rede",
+        descricao: "Configurar alertas para trafego em tempo real",
+        dataDeVencimento: "18/12",
+    },
+    {
+        id: 3,
+        titulo: "Atualizacao de software",
+        descricao: "Aplicar patches",
+        dataDeVencimento: "22/12",
+    },
+];
 
-console.log(dataDeVencimento.length)
+function print(texto) {
+    return console.log(texto)
+}
+
+const novaAtividade = new Map();
+
+for (const atividade of atividades) {
+    const { id } = atividade;
+    novaAtividade.set(id, { ...atividade });
+}
+
+const descricao = (novaAtividade.get(3)).descricao;
+const descricaoLenght = descricao.length;
+const dataDeVencimento = (novaAtividade.get(3)).dataDeVencimento;
+
+function verificarDescricaoLenght(descricaoLenght, descricao, dataDeVencimento) {
+    if (descricaoLenght > 50) {
+        return ('Descricao ultrapassa limite de caracteres.');
+    }
+    if (descricaoLenght <= 50) {
+        return (`${descricao} ate ${dataDeVencimento}`);
+    }
+}
+
+print(verificarDescricaoLenght(descricaoLenght, descricao, dataDeVencimento));
